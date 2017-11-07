3b2791a4b806d99f7771d50c8654df089e59bae4
Bea Esguerra <bea.esguerra@hotmail.com>
2017-11-07 00:51:46 -0700

    DB-5 fix typo in dockerize script

--- dockerize.sh
+++ dockerize.sh
@@ -6,7 +6,7 @@ if [ "$1" == "init" ]; then
     docker build -t $IMAGE_NAME .
 elif [ "$1" == "build" ]; then
     docker run --rm --volume "$(pwd):$DOCKER_HOME" -ti $IMAGE_NAME yarn install --frozen-lockfile --no-bin-links
-    docker run --rm --volume "$(pwd):$DOCKER_HOME" $IMAGE_NAME npm runs build
+    docker run --rm --volume "$(pwd):$DOCKER_HOME" $IMAGE_NAME npm run build
 elif [ "$1" == "run" ]; then
     docker run --rm --volume "$(pwd):$DOCKER_HOME" -ti $IMAGE_NAME yarn install --frozen-lockfile --no-bin-links
     docker run --rm --volume "$(pwd):$DOCKER_HOME" -ti -p 8080:8080 $IMAGE_NAME npm start
