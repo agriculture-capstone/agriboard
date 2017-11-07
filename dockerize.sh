fa5715697f7fb36eaee36b6b7cd22c91f82258f0
Bea Esguerra <bea.esguerra@hotmail.com>
2017-11-07 00:46:49 -0700

    DB-5 removing package-lock file

--- dockerize.sh
+++ dockerize.sh
@@ -3,13 +3,13 @@ IMAGE_NAME="boresha/dashboard"
 DOCKER_HOME="/dashboard/"
 
 if [ "$1" == "init" ]; then
-	docker build -t $IMAGE_NAME .
+    docker build -t $IMAGE_NAME .
 elif [ "$1" == "build" ]; then
-	docker run --rm --volume "$(pwd):/$DOCKER_HOME/" -ti $IMAGE_NAME yarn install --frozen-lockfile --no-bin-links
-	docker run --rm --volume "$(pwd):/$DOCKER_HOME/" $IMAGE_NAME npm runs build
+    docker run --rm --volume "$(pwd):/$DOCKER_HOME/" -ti $IMAGE_NAME yarn install --frozen-lockfile --no-bin-links
+    docker run --rm --volume "$(pwd):/$DOCKER_HOME/" $IMAGE_NAME npm runs build
 elif [ "$1" == "run" ]; then
     docker run --rm --volume "$(pwd):/$DOCKER_HOME/" -ti $IMAGE_NAME yarn install --frozen-lockfile --no-bin-links
     docker run --rm --volume "$(pwd):/$DOCKER_HOME/" -ti -p 8080:8080 $IMAGE_NAME npm start
 else
-	echo "usage: docker_build [init | build | run]"
+    echo "usage: docker_build [init | build | run]"
 fi
