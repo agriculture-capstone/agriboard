bae6cf2d7240769e1bc0f67b03c9474fadf990db
Enoch Tsang <etsang1@hotmail.com>
2017-11-22 18:03:27 -0700

    b

--- dockerize.sh
+++ dockerize.sh
@@ -9,14 +9,14 @@ DOCKER_HOME="/agriboard/"
 if [ "$1" == "init" ]; then
     docker build -t $IMAGE_NAME .
 elif [ "$1" == "build" ]; then
-    docker run --rm --volume "$DIR:$DOCKER_HOME" $IMAGE_NAME yarn install --frozen-lockfile
-    docker run --rm --volume "$DIR:$DOCKER_HOME" $IMAGE_NAME npm run build
-elif [ "$1" == "run" ]; then
     echo "DOCKER_HOME is "
     echo $DOCKER_HOME
     echo "DIR is "
     echo $DIR
     docker run --rm --volume "$DIR:$DOCKER_HOME" $IMAGE_NAME yarn install --frozen-lockfile
+    docker run --rm --volume "$DIR:$DOCKER_HOME" $IMAGE_NAME npm run build
+elif [ "$1" == "run" ]; then
+   docker run --rm --volume "$DIR:$DOCKER_HOME" $IMAGE_NAME yarn install --frozen-lockfile
     docker run --rm --volume "$DIR:$DOCKER_HOME" -p 8080:8080 $IMAGE_NAME npm start
 else
     echo "usage: docker_build [init | build | run]"
