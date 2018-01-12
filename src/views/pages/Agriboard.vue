cdccdc2645251940dc4f20753ab54716fa1f1ddb
Enoch Tsang <etsang1@hotmail.com>
2018-01-12 10:01:49 -0700

    remove default vue content

---
+++ src/views/pages/Agriboard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="Agriboard">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Agriboard',
+  methods: {
+    test() {
+      return 'hi';
+    },
+  },
+  data () {
+    return {
+      msg: 'Agriboard',
+    };
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
