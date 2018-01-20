a4d6bbb7720d7fe42d2f105712b71a117ae00dfc
Enoch Tsang <etsang1@hotmail.com>
2018-01-19 19:11:09 -0700

    ab-34 add login page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/views/pages/Home/index.vue';
+import Login from '@/views/pages/Login/index.vue';
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
     },
   ],
 });
---
+++ src/views/pages/Login/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="Login">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Login',
+  methods: {
+    test() {
+      return 'hi';
+    },
+  },
+  data () {
+    return {
+      msg: 'Login',
+    };
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
