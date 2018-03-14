153072725eed59dbe4d2d20083a647c37ea49d02
Bea Esguerra <bea.esguerra@hotmail.com>
2018-03-14 04:20:50 -0600

    reverting font change

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@ export default Vue.extend({
 
 body {
   #app {
-    // font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
