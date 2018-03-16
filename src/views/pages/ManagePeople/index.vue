f89503e631bb33e3923edfff588bb1efe87eaf12
Moath <mfalthaw@ucalgary.ca>
2018-03-16 00:05:41 -0600

    AB-70 init styling

--- src/views/pages/ManagePeople/index.vue
+++ src/views/pages/ManagePeople/index.vue
@@ -29,28 +29,46 @@
       
       <md-card class="md-layout-item md-size-50 md-small-size-100">
         <md-card-content>
-          <md-field>
-            <label>First Name</label>
-            <md-input v-model="form.firstName" name="first-name" id="first-name" autocomplete="given-name" />
-          </md-field>
-          <md-field>
-            <label>Last Name</label>
-            <md-input v-model="form.lastName" name="last-name" id="last-name" autocomplete="last-name" />
-          </md-field>
-          <md-field>
-            <label>Phone Number</label>
-            <md-input v-model="form.phoneNumber" name="phone-number" id="phone-number" autocomplete="phone-number" />
-          </md-field>
-          <md-field>
-            <label>Email</label>
-            <md-input v-model="form.email" name="email" id="email" autocomplete="email" />
-          </md-field>
-          <md-field>
-            <md-select v-model="form.type" name="user-type" id="user-type" placeholder="User Type">
-              <md-option value="farmer">Farmer</md-option>
-              <md-option value="trader">Trader</md-option>
-            </md-select>
-          </md-field>
+          
+          <div class="md-layout md-gutter">
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label>First Name</label>
+                <md-input v-model="form.firstName" name="first-name" id="first-name" autocomplete="given-name" />
+              </md-field>
+            </div>
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label>Last Name</label>
+                <md-input v-model="form.lastName" name="last-name" id="last-name" autocomplete="last-name" />
+              </md-field>
+            </div>
+          </div>
+
+          <div class="md-layout md-gutter">
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label>Phone Number</label>
+                <md-input v-model="form.phoneNumber" name="phone-number" id="phone-number" autocomplete="phone-number" />
+              </md-field>
+            </div>
+            <div class="md-layout-item md-small-size-100">
+              <md-field>
+                <label>Email</label>
+                <md-input v-model="form.email" name="email" id="email" autocomplete="email" />
+              </md-field>
+            </div>
+          </div>
+
+          <div class="md-layout md-gutter">
+            <md-field>
+              <md-select v-model="form.type" name="user-type" id="user-type" placeholder="User Type">
+                <md-option value="farmer">Farmer</md-option>
+                <md-option value="trader">Trader</md-option>
+              </md-select>
+            </md-field>
+          </div>
+
         </md-card-content>
       </md-card>
 
