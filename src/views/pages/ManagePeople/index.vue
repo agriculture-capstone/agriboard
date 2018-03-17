29a738a7f9b7c20a214c0a1bc6f9459f4b7d065b
mfalthaw <mfalthaw@ucalgary.ca>
2018-03-17 00:39:39 -0600

    AB-70 user type text

--- src/views/pages/ManagePeople/index.vue
+++ src/views/pages/ManagePeople/index.vue
@@ -64,9 +64,12 @@
             </div>
 
             <div class="md-layout md-gutter">
-              <md-radio v-model="radio" value="farmer">Farmer</md-radio>
-              <md-radio v-model="radio" value="trader">Trader</md-radio>
-              <md-radio v-model="radio" value="admin">Admin</md-radio>
+              <div class="md-card-header">User Type:</div>
+              <div class="md-layout-item md-small-size-100">
+                <md-radio v-model="radio" value="farmer">Farmer</md-radio>
+                <md-radio v-model="radio" value="trader">Trader</md-radio>
+                <md-radio v-model="radio" value="admin">Admin</md-radio>
+              </div>
             </div>
           </md-card-content>
         </md-card>
