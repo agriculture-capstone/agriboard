efd487764deaeaaaf2f56aa43a18a70115dbcec3
mfalthaw <mfalthaw@ucalgary.ca>
2018-03-16 22:12:23 -0600

    AB-70 fix fab issue

--- src/views/pages/ManagePeople/index.vue
+++ src/views/pages/ManagePeople/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class='ManagePeople'>
-
+    <div class="add-button">
+      <md-button @click="showAddDialog = true" class="md-fab md-primary md-fab-bottom-right md-fixed add-user-button">
+        <md-icon>add</md-icon>
+      </md-button>
+    </div>
     <md-table v-model="searched" md-sort="name" md-sort-order="asc" md-card @md-selected="onSelect">
       <md-table-toolbar>
         <div class="md-toolbar-section-start">
@@ -12,11 +16,7 @@
       </md-table-toolbar>
 
       <h2 class="error md-subheader">{{error}}</h2>
-      <div class="add-button">
-        <md-button @click="showAddDialog = true" class="md-fab md-primary md-fab-bottom-right md-fixed add-user-button">
-          <md-icon>add</md-icon>
-        </md-button>
-      </div>
+
       <md-table-row slot="md-table-row" slot-scope="{ item }" md-selectable="single" md-auto-select>
         <md-table-cell md-label="Name" md-sort-by="name">{{ item.name }}</md-table-cell>
         <md-table-cell md-label="Phone Number" md-sort-by="phoneNumber">{{ item.phoneNumber }}</md-table-cell>
@@ -228,6 +228,11 @@ export default Vue.extend({
     height: 100%;
     display: block;
   }
+}
+
+.add-button {
+  position:relative;
+  z-index:1000;
 }
 
 .error {
