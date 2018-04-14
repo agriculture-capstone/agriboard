1fae6de135ecb3d47fad482d6a91d1755d203486
James Inglis <jamesekinglis@gmail.com>
2018-04-14 10:21:48 -0600

    merging master

--- src/utils/createCoreModule/utils.ts
+++ src/utils/createCoreModule/utils.ts
@@ -2,6 +2,7 @@ import * as R from 'ramda';
 import { CoreModuleName } from '@/utils/createCoreModule';
 import { CorePath } from '@/utils/CoreAPI';
 import { StoreRow } from '@/store/types';
+import { AuthorizationError } from '@/errors/AuthorizationError';
 
 /** Type for object with UUID */
 type ObjectWithUUID<T extends object> = T & { uuid: string };
@@ -21,7 +22,21 @@ export function isResponse(response: any): response is Response {
   return (response instanceof Response);
 }
 
+/**
+ * Enum for core module names
+ */
+enum CORE_MODULE_NAMES {
+  FARMER = 'farmer',
+  MILK = 'milk',
+  DELIVERY = 'delivery',
+  TRADER = 'trader',
+  ADMIN = 'admin',
+  MEMO = 'memo',
+  MONITOR = 'monitor',
+  LOANS = 'loan',
+}
 
+<<<<<<< HEAD
 /** Map for modules */
 // tslint:disable-next-line:variable-name
 export const CoreModuleNames: CoreModuleName[] = [
@@ -34,6 +49,39 @@ export const CoreModuleNames: CoreModuleName[] = [
   'loan',
   'payment',
 ];
+=======
+/**
+ * Gets the core module names for a certain user type
+ * @param userType type of user to get core modules for
+ */
+export function getPermittedCoreModuleNames(userType: string) {
+  let permittedCoreModules: CoreModuleName[] = [];
+  if (userType === 'admins') {
+    permittedCoreModules = [
+      CORE_MODULE_NAMES.FARMER,
+      CORE_MODULE_NAMES.TRADER,
+      CORE_MODULE_NAMES.ADMIN,
+      CORE_MODULE_NAMES.MEMO,
+      CORE_MODULE_NAMES.MONITOR,
+      CORE_MODULE_NAMES.MILK,
+      CORE_MODULE_NAMES.DELIVERY,
+      // CORE_MODULE_NAMES.LOANS, TODO
+    ];
+  } else if (userType === 'monitors') {
+    permittedCoreModules = [
+      CORE_MODULE_NAMES.FARMER,
+      CORE_MODULE_NAMES.TRADER,
+      CORE_MODULE_NAMES.MEMO,
+      CORE_MODULE_NAMES.MILK,
+      CORE_MODULE_NAMES.DELIVERY,
+      // CORE_MODULE_NAMES.LOANS, TODO
+    ];
+  } else {
+    throw new AuthorizationError(`Invalid user type: ${userType}`);
+  }
+  return permittedCoreModules;
+}
+>>>>>>> master
 
 /**
  * Get the module path for the specified module
@@ -47,6 +95,8 @@ export function getModulePath(module: CoreModuleName): CorePath {
     case 'milk': return '/transactions/products/milk';
 
     case 'delivery': return '/productExports';
+
+    case 'memo': return '/memos';
 
     case 'loan': return '/transactions/money/loans';
 
